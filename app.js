9861fd1128161e7c32e6b7ea1555eba6c20ebfcc
Tanandara <tanandara2014@gmail.com>
2018-07-28 00:00:23 +0700

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.post('/autenticate', (req, res) => res.json({
+	"access_token" : "xxxxxxxxxxxxxxxxxxxxxx",
+	"token_type" : "bearer",
+	"expires_in" : 5555,
+	"refresh_token" : "xxxxxxxxxxxxxxxxxxxxxxx"
+}))
+
+app.post('/signpdf', (req, res) => res.json({
+  "base64Pdf": "xxxxxxxxxxxxxxx",
+  "message": "Message",
+  "messageCode": 999
+}))
+
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
